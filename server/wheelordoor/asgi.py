e9100746fcdaf439e7a9c7051aa16df4644db145
Cassius Close <CassiusClose@gmail.com>
2022-03-17 20:51:16 -0400

    Initial sockets setup

---
+++ server/wheelordoor/asgi.py
@@ -0,0 +1,22 @@
+import os
+
+import django
+from channels.http import AsgiHandler
+from channels.routing import ProtocolTypeRouter, get_default_application, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from channels.auth import AuthMiddlewareStack
+from django.urls import path
+
+from . import consumers 
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wheelordoor.settings')
+django.setup()
+
+application = ProtocolTypeRouter({
+    "http": AsgiHandler(),
+    "websocket": AllowedHostsOriginValidator(AuthMiddlewareStack(
+        URLRouter([
+            path('socket-test', consumers.WODConsumer())
+        ])
+    )),
+})
