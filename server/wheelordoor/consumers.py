85d20241383e221a02dcb5bbd6693d065765259d
Cassius Close <CassiusClose@gmail.com>
2022-03-18 22:38:49 -0400

    Added connection number tracking

--- server/wheelordoor/consumers.py
+++ server/wheelordoor/consumers.py
@@ -2,7 +2,7 @@ import asyncio
 import json
 from channels.consumer import AsyncConsumer
 
-from wod.models import inc_wheels, inc_doors
+from wod.models import inc_wheels, inc_doors, inc_conn, dec_conn
 from asgiref.sync import async_to_sync
 
 class WODConsumer(AsyncConsumer):
@@ -11,6 +11,8 @@ class WODConsumer(AsyncConsumer):
         await self.channel_layer.group_add("default", self.channel_name)
 
         await self.send({"type": "websocket.accept", })
+
+        await inc_conn()
 
 
     async def websocket_receive(self, event):
@@ -26,9 +28,15 @@ class WODConsumer(AsyncConsumer):
 
 
     async def websocket_disconnect(self, event):
+        await dec_conn()
         await self.channel_layer.group_discard("default", self.channel_name)
 
 
-    async def update(self, event):
-        j = {"percentage": event.get("percentage")}
+    async def data_update(self, event):
+        j = {"type": "data", "percentage": event.get("percentage")}
+        await self.send({"type": "websocket.send", "text": json.dumps(j)})
+
+
+    async def connection_update(self, event):
+        j = {"type": "connections", "connections": event.get("connections")}
         await self.send({"type": "websocket.send", "text": json.dumps(j)})
--- server/wod/models.py
+++ server/wod/models.py
@@ -6,15 +6,50 @@ class Results(models.Model):
     wheels = models.IntegerField(default=0)
     doors = models.IntegerField(default=0)
     percentage = models.FloatField(default=0)
+    connections = models.IntegerField(default=0)
 
 
-@sync_to_async
-def inc(wheel):
+def getResultsObject():
     if Results.objects.count() == 0:
         newr = Results.objects.create()
         newr.save()
 
-    r = Results.objects.all().first()
+    r = Results.objects.all().first() 
+    return r
+
+
+
+@sync_to_async
+def change_conn(inc):
+    r = getResultsObject()
+
+    if inc:
+        r.connections += 1
+    else:
+        r.connections -= 1
+    r.save()
+
+    return r.connections
+
+
+async def inc_conn():
+    conn = await change_conn(True)
+
+    channel_layer = get_channel_layer()
+    await channel_layer.group_send("default", {"type": "connection_update", "connections": conn})
+
+
+async def dec_conn():
+    conn = await change_conn(False)
+
+    channel_layer = get_channel_layer()
+    await channel_layer.group_send("default", {"type": "connection_update", "connections": conn})
+
+
+@sync_to_async
+def inc(wheel):
+    r = getResultsObject()
+
     if wheel:
         r.wheels += 1
     else:
@@ -22,19 +57,20 @@ def inc(wheel):
     r.percentage = r.wheels / (r.wheels + r.doors)
     r.save()
 
-    return r.percentage
+    return (r.wheels, r.doors)
+
 
 
 async def inc_wheels():
-    perc = await inc(True)
+    (wheels, doors) = await inc(True)
 
     channel_layer = get_channel_layer()
-    await channel_layer.group_send("default", {"type": "update", "percentage": perc})
+    await channel_layer.group_send("default", {"type": "data_update", "wheels": wheels, "doors": doors})
 
 
 
 async def inc_doors():
-    perc = await inc(False)
+    (wheels, doors) = await inc(False)
 
     channel_layer = get_channel_layer()
-    await channel_layer.group_send("default", {"type": "update", "percentage": perc})
+    await channel_layer.group_send("default", {"type": "data_update", "wheels": wheels, "doors": doors})
